1893c2ff1110e6b47fcbcbf33e9373aa15332d8c
Roman Yatman <machlestv@gmail.com>
2022-02-24 00:20:35 +0700

    Initial commit

---
+++ back/dormsys/src/main/java/ru/nsu/yattroman/dormsys/DormsysApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.yattroman.dormsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormsysApplication.class, args);
+	}
+
+}
